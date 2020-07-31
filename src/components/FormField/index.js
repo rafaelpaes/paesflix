1865e83b205d0ecd50d44bbbea549b975b1ce375
Rafael Paes <rafaelfelipepaes@gmail.com>
2020-07-31 16:05:24 -0300

    Conclusão do Concurso

--- src/components/FormField/index.js
+++ src/components/FormField/index.js
@@ -70,11 +70,14 @@ const Input = styled.input`
 `;
 
 function FormField({
-  label, type, name, value, onChange,
+  label, type, name, value, onChange, suggestions,
 }) {
   const fieldId = `id_${name}`;
   const isTextArea = type === 'textarea';
   const tag = isTextArea ? 'textarea' : 'input';
+
+  const hasValue = Boolean(value.length);
+  const hasSuggestions = Boolean(suggestions.length);
 
   return (
     <FormFieldWrapper>
@@ -86,13 +89,29 @@ function FormField({
           id={fieldId}
           type={type}
           name={name}
+          hasValue={hasValue}
           value={value}
           onChange={onChange}
+          autoComplete={hasSuggestions ? 'off' : 'on'}
+          list={hasSuggestions ? `suggestionFor_${fieldId}` : 'undefined'}
         />
         <Label.Text>
           {label}
           :
         </Label.Text>
+        {
+          hasSuggestions && (
+            <datalist id={`suggestionFor_${fieldId}`}>
+              {
+                suggestions.map((suggestion) => (
+                  <option value={suggestion} key={`suggestionFor_${fieldId}_option${suggestion}`}>
+                    {suggestion}
+                  </option>
+                ))
+              }
+            </datalist>
+          )
+        }
       </Label>
     </FormFieldWrapper>
   );
@@ -102,6 +121,7 @@ FormField.defaultProps = {
   type: 'text',
   value: '',
   onChange: () => {},
+  suggestions: [],
 };
 
 FormField.propTypes = {
@@ -110,6 +130,7 @@ FormField.propTypes = {
   name: PropTypes.string.isRequired,
   value: PropTypes.string,
   onChange: PropTypes.func,
+  suggestions: PropTypes.arrayOf(PropTypes.string),
 };
 
 export default FormField;
--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -1,18 +1,28 @@
-import React from 'react';
+/* eslint-disable max-len */
+import React, { useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
 import useForm from '../../../hooks/useForm';
 import Button from '../../../components/Button';
 import videosRepository from '../../../repositories/videos';
+import categoriasRepository from '../../../repositories/categorias';
 
 function CadastroVideo() {
   const history = useHistory();
+  const [categorias, setCategorias] = useState([]);
+  const categoryTitles = categorias.map(({ titulo }) => titulo);
   const { handleChange, values } = useForm({
     titulo: 'Vídeo padrão',
-    url: 'https://www.youtube.com/watch?time_continue=3129&v=hhQ3RtvmfEg&feature=emb_logo',
-    categoria: 'Front End',
+    url: 'https://www.youtube.com/watch?v=9NZrdgazaps',
+    categoria: 'Back End',
   });
+
+  useEffect(() => {
+    categoriasRepository.getAll().then((categoriasFromServer) => {
+      setCategorias(categoriasFromServer);
+    });
+  }, []);
 
   return (
     <PageDefault>
@@ -21,10 +31,14 @@ function CadastroVideo() {
       <form onSubmit={(event) => {
         event.preventDefault();
 
+        const categoriaEscolhida = categorias.find((categoria) => categoria.titulo === values.categoria);
+        // eslint-disable-next-line no-console
+        console.log(categoriaEscolhida);
+
         videosRepository.create({
           titulo: values.titulo,
           url: values.url,
-          categoriaId: 1,
+          categoriaId: categoriaEscolhida.id,
         }).then(() => {
           // eslint-disable-next-line no-console
           console.log('Cadastrou com sucesso!');
@@ -51,6 +65,7 @@ function CadastroVideo() {
           name="categoria"
           value={values.categoria}
           onChange={handleChange}
+          suggestions={categoryTitles}
         />
 
         <Button type="submit">
